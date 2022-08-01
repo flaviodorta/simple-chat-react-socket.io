51cb7457ba2fe88a41940425cb33a41c9392806c
flaviodorta <dorta.dev@gmail.com>
2022-08-01 02:19:59 -0300

    fix: change useState to useRef to save socket reference

--- client/src/hooks/useChatRoom.ts
+++ client/src/hooks/useChatRoom.ts
@@ -1,12 +1,13 @@
 import { useState } from 'react';
 
 interface Message {
+  socketId: string;
+  username: string;
   content: string;
-  ownerName: string;
-  ownerId: string;
-  date: Date;
 }
 
 export function useChatRoom() {
   const [messages, setMessages] = useState<Message[]>([]);
+
+  return [messages, setMessages];
 }
--- client/src/hooks/useSocketIo.ts
+++ client/src/hooks/useSocketIo.ts
@@ -1,22 +1,20 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef } from 'react';
 import { io, Socket } from 'socket.io-client';
 import { RECONNECT_DELAY, URL_SERVER } from '../utils/constants';
 
 export function useSocketIo() {
-  const [socket, setSocket] = useState<Socket | null>(null);
+  const socket = useRef<Socket | null>(null);
 
   useEffect(() => {
-    if (!socket) {
-      const connectedSocket = io(URL_SERVER);
-      setSocket(connectedSocket);
-      connectedSocket.on('connect', () => {
-        console.log(connectedSocket.id);
-      });
-      connectedSocket.on('connect_error', () => {
-        setTimeout(() => connectedSocket.connect(), RECONNECT_DELAY);
-      });
-    }
-  }, [socket]);
+    const connectedSocket = io(URL_SERVER);
+    socket.current = connectedSocket;
+    connectedSocket.on('connect', () => {
+      console.log(connectedSocket.id);
+    });
+    connectedSocket.on('connect_error', () => {
+      setTimeout(() => connectedSocket.connect(), RECONNECT_DELAY);
+    });
+  }, []);
 
-  return socket;
+  return socket.current;
 }
